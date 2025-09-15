beea707cd4074a2a3da3464bdaf0d69cef3e9360
NicssLucik02 <nikitalucenko1@gmail.com>
2025-09-15 19:35:49 +0300

    update 2

--- src/components/Header/Menu/Menu.tsx
+++ src/components/Header/Menu/Menu.tsx
@@ -15,51 +15,48 @@ type Props = {
 export const Menu: React.FC<Props> = ({ isOpenedMenu, navLinks }) => {
   return (
     <div className={`burger__menu-overlay ${isOpenedMenu ? 'open' : ''}`}>
-      {isOpenedMenu && (
-        <div className="burger__menu">
-          <nav>
-            <ul className="burger__menu-lists">
-              {navLinks.map((link) => {
-                return (
-                  <li key={link.path}>
-                    <Link
-                      className="burger__menu-link uppercase-text"
-                      href={link.path}
-                    >
-                      {link.title}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
-          <div className="menu__footer">
-            <div className="menu__footer-favourites">
-              <Link href="h">
-                <Image
-                  src="/images/heart.svg"
-                  width={16}
-                  height={16}
-                  alt="favourites"
-                  className="header__icon"
-                />
-              </Link>
-            </div>
+      <div className="burger__menu">
+        <nav>
+          <ul className="burger__menu-lists">
+            {navLinks.map((link) => (
+              <li key={link.path}>
+                <Link
+                  className="burger__menu-link uppercase-text"
+                  href={link.path}
+                >
+                  {link.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
 
-            <div className="menu__footer-cart">
-              <Link href="h">
-                <Image
-                  src="/images/shop-cart.svg"
-                  width={16}
-                  height={16}
-                  alt="cart"
-                  className="header__icon"
-                />
-              </Link>
-            </div>
+        <div className="menu__footer">
+          <div className="menu__footer-favourites">
+            <Link href="h">
+              <Image
+                src="/images/heart.svg"
+                width={16}
+                height={16}
+                alt="favourites"
+                className="header__icon"
+              />
+            </Link>
+          </div>
+
+          <div className="menu__footer-cart">
+            <Link href="h">
+              <Image
+                src="/images/shop-cart.svg"
+                width={16}
+                height={16}
+                alt="cart"
+                className="header__icon"
+              />
+            </Link>
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
