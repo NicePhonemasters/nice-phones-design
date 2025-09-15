6a00079ef0066da18fca618f7a41134e553ae8c2
NicssLucik02 <nikitalucenko1@gmail.com>
2025-09-15 18:04:39 +0300

    update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,10 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
 import './header.scss';
 import '@styles/fonts.scss';
+import { useState } from 'react';
+import { Menu } from './Menu/Menu';
 
 export const Header = () => {
   const navLinks = [
@@ -10,70 +13,85 @@ export const Header = () => {
     { title: 'Tablets', path: '/tablets' },
     { title: 'Accessories', path: '/accessories' },
   ];
+  const [isOpenedMenu, setIsOpenedMenu] = useState(false);
 
   return (
-    <header className="header">
-      <Link href="/">
-        <Image
-          width={80}
-          height={26}
-          src="/images/logo.svg"
-          className="header__logo"
-          alt="Nice Gadgets"
-        />
-      </Link>
-
-      <nav className="header__nav">
-        <ul className="header__lists">
-          {navLinks.map((link) => {
-            return (
-              <li key={link.path}>
-                <Link
-                  className="header__nav-link uppercase-text"
-                  href={link.path}
-                >
-                  {link.title}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-
-      <div className="header__right">
-        <div className="header__right-favourites is-active">
-          <Link href="h">
-            <Image
-              src="/images/heart.svg"
-              width={16}
-              height={16}
-              alt="favourites"
-            />
-          </Link>
-        </div>
-
-        <div className="header__right-cart">
-          <Link href="h">
-            <Image
-              src="/images/shop-cart.svg"
-              width={16}
-              height={16}
-              alt="cart"
-            />
-          </Link>
-        </div>
-
-        <div className="header__burger-menu">
+    <>
+      <header className="header">
+        <Link href="/">
           <Image
-            src="/images/burger-menu.svg"
-            width={16}
-            height={16}
-            alt="burger-menu"
+            width={80}
+            height={26}
+            src="/images/logo.svg"
+            className="header__logo"
+            alt="Nice Gadgets"
           />
-        </div>
+        </Link>
 
-        <div></div>
-      </div>
-    </header>
+        <nav className="header__nav">
+          <ul className="header__lists">
+            {navLinks.map((link) => {
+              return (
+                <li key={link.path}>
+                  <Link
+                    className="header__nav-link uppercase-text"
+                    href={link.path}
+                  >
+                    {link.title}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+
+        <div className="header__right">
+          <div className="header__right-favourites is-active">
+            <Link href="h">
+              <Image
+                src="/images/heart.svg"
+                width={16}
+                height={16}
+                alt="favourites"
+                className="header__icon"
+              />
+            </Link>
+          </div>
+
+          <div className="header__right-cart">
+            <Link href="h">
+              <Image
+                src="/images/shop-cart.svg"
+                width={16}
+                height={16}
+                alt="cart"
+              />
+            </Link>
+          </div>
+
+          <div className="header__burger-menu">
+            {!isOpenedMenu ? (
+              <Image
+                src="/images/burger-menu.svg"
+                width={16}
+                height={16}
+                alt="burger-menu"
+                onClick={() => setIsOpenedMenu(true)}
+              />
+            ) : (
+              <Image
+                src="/images/close.svg"
+                width={16}
+                height={16}
+                alt="burger-menu"
+                onClick={() => setIsOpenedMenu(false)}
+              />
+            )}
+          </div>
+        </div>
+      </header>
+
+      <Menu isOpenedMenu={isOpenedMenu} navLinks={navLinks} />
+    </>
   );
 };
