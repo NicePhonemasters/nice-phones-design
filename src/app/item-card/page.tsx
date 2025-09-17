94dc57e3937ca7e5607f5e0279e4104a6f370c89
iulii sobol <soboliulii@gmail.com>
2025-09-17 16:19:03 +0300

    feat_responsive

--- src/app/item-card/page.tsx
+++ src/app/item-card/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import '../../styles/itemCard.scss';
+import '../../styles/itemCard/itemCard.scss';
 import Image from 'next/image';
 
 const itemImages = [
@@ -11,33 +11,28 @@ const itemImages = [
   '/iphone-hero.png',
 ];
 
-// const colors = [
-//   { id: "1", name: "Gold", image: "/iphone-gold.png" },
-//   { id: "2", name: "Silver", image: "/iphone-silver.png" },
-//   { id: "3", name: "Green", image: "/iphone-green.png" },
-//   { id: "4", name: "Black", image: "/iphone-black.png" },
-// ];
-
 export function ItemCard() {
   return (
     <div className="container_item_card">
       <section className="item-card-section item-card-section-main">
+        <h3 className="item_card__title">
+          Apple iPhone 11 Pro Max 64GB Gold (iMT9G2FS/A)
+        </h3>
         <div className="item_card">
-          <h3 className="item_card__title">
-            Apple iPhone 11 Pro Max 64GB Gold (iMT9G2FS/A)
-          </h3>
           <div className="item_card__image">
             <Image
+              className="item_card__image-hero"
               src="/iphone-hero.png"
               alt="main-image"
-              width={288}
-              height={288}
+              width={275}
+              height={275}
             />
           </div>
           <ul className="item_card__image-list">
             {itemImages.map((image, i) => (
               <li className="item_card__image-list-item" key={i}>
                 <Image
+                  className="item_card__image-small"
                   src={image}
                   alt={`item-image-${i + 1}`}
                   width={50}
@@ -49,8 +44,10 @@ export function ItemCard() {
           <div className="item_card__info">
             <div className="item_card__info-colour-section">
               <div className="item_card__info-colour-section__text">
-                <p>Available colors</p>
-                <p>ID: 802390</p>
+                <p className="item_card__info-colour-section__label">
+                  Available colors
+                </p>
+                <p className="item_card__info-colour-section__id">ID: 802390</p>
               </div>
               <div className="item_card__info-colour-section__color">
                 <ul className="item_card__info-colour-section__color-list">
@@ -114,7 +111,7 @@ export function ItemCard() {
                 type="button"
                 className="item_card__info-buy-section-button-buy"
               >
-                Add to cart
+                {/* Add to cart */}
               </button>
               <button
                 type="button"
@@ -151,10 +148,11 @@ export function ItemCard() {
           </h5>
           <p className="item-card-description__text">
             A transformative triple-camera system that adds tons of capability
-            without complexity. An unprecedented leap in battery life. And a
-            mind-blowing chip that doubles down on machine learning and pushes
-            the boundaries of what a smartphone can do. Welcome to the first
-            iPhone powerful enough to be called Pro.
+            without complexity. <br />
+            An unprecedented leap in battery life. And a mind-blowing chip that
+            doubles down on machine learning and pushes the boundaries of what a
+            smartphone can do. Welcome to the first iPhone powerful enough to be
+            called Pro.
           </p>
           <h5 className="item-card-description__subtitle">Camera</h5>
           <p className="item-card-description__text">
@@ -180,9 +178,94 @@ export function ItemCard() {
         </div>
         <div className="item-card-description__tech-specs">
           <h4 className="item-card-description__tech-spec-title">Tech specs</h4>
+          <div className="item-card-description__tech-specs-list">
+            <div className="item-card-description__tech-specs-row">
+              <div className="item-card-description__tech-specs-name">
+                Screen
+              </div>
+              <div className="item-card-description__tech-specs-value">
+                6.5” OLED
+              </div>
+            </div>
+
+            <div className="item-card-description__tech-specs-row">
+              <div className="item-card-description__tech-specs-name">
+                Resolution
+              </div>
+              <div className="item-card-description__tech-specs-value">
+                2688x1242
+              </div>
+            </div>
+
+            <div className="item-card-description__tech-specs-row">
+              <div className="item-card-description__tech-specs-name">
+                Processor
+              </div>
+              <div className="item-card-description__tech-specs-value">
+                Apple A12 Bionic
+              </div>
+            </div>
+
+            <div className="item-card-description__tech-specs-row">
+              <div className="item-card-description__tech-specs-name">RAM</div>
+              <div className="item-card-description__tech-specs-value">
+                3 GB
+              </div>
+            </div>
+
+            <div className="item-card-description__tech-specs-row">
+              <div className="item-card-description__tech-specs-name">
+                Built in memory
+              </div>
+              <div className="item-card-description__tech-specs-value">
+                64 GB
+              </div>
+            </div>
+
+            <div className="item-card-description__tech-specs-row">
+              <div className="item-card-description__tech-specs-name">
+                Camera
+              </div>
+              <div className="item-card-description__tech-specs-value">
+                12 Mp + 12 Mp + 12 Mp (Triple)
+              </div>
+            </div>
+
+            <div className="item-card-description__tech-specs-row">
+              <div className="item-card-description__tech-specs-name">Zoom</div>
+              <div className="item-card-description__tech-specs-value">
+                Optical, 2x
+              </div>
+            </div>
+
+            <div className="item-card-description__tech-specs-row">
+              <div className="item-card-description__tech-specs-name">Cell</div>
+              <div className="item-card-description__tech-specs-value">
+                GSM, LTE, UMTS
+              </div>
+            </div>
+          </div>
         </div>
       </section>
-      <section className="item-card-section item-card-section-favourites"></section>
+      <section className="item-card-section item-card-favourites">
+        <div className="item-card-favourites__title-and-buttons">
+          <h3 className="item-card-favourites__title">You may also like</h3>
+          <div className="item-card-favourites__buttons">
+            <button className="item-card-favourites__buttons-prev"></button>
+            <button className="item-card-favourites__buttons-next"></button>
+          </div>
+        </div>
+        <div className="item-card-favourites__slider">
+          <div
+            className="placeholder"
+            style={{
+              backgroundColor: 'var(--color-placeholder)',
+              width: '100%',
+              height: '30vh',
+            }}
+          ></div>
+        </div>
+      </section>
     </div>
   );
 }
