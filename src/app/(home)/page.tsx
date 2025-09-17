245b0af87fb81c329971c4fc8cb787b12440291a
Dima Ilchenko <dima.ilchenko.ol@gmail.com>
2025-09-17 14:08:27 +0200

    SCRUM-11 delete my components from main component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,5 @@
-import Cart from 'app/cart/Page';
-import Catalog from 'app/catalog/Page';
-import Favourites from 'app/favorites/favourites';
-
 function App() {
-  return (
-    <main>
-      <Cart />
-      <Catalog />
-      <Favourites />
-    </main>
-  );
+  return <main></main>;
 }
 
 export default App;
