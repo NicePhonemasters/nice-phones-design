e62decb530a2086206f8291b659d41f5886615eb
Maxim <makson202007@gmail.com>
2025-09-17 15:22:20 +0300

    feat: added svg support in config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  webpack(config) {
+    // Add support for importing SVGs as React components
+    config.module.rules.push({
+      test: /\.svg$/, // Match .svg files
+      issuer: /\.[jt]sx?$/, // Only apply to JS/TS files
+      use: ['@svgr/webpack'], // Use SVGR to transform SVG into React component
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
