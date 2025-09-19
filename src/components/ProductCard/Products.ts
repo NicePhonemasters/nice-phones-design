c4f3db0752ee5e1c8d8ea5a3fc1b9d3a36097e21
Назар ⠀ <nrxyouh@gmail.com>
2025-09-19 15:56:51 +0300

    Product Card Adaptive with mixin

--- src/components/ProductCard/Products.ts
+++ src/components/ProductCard/Products.ts
@@ -9,14 +9,4 @@ export const product = [
     ram: '4GB',
     imageSrc: '/img/phones/apple-iphone-xs/gold/00.webp',
   },
-  {
-    id: '2',
-    title: 'Apple iPhone 13 128GB Midnight',
-    price: 999,
-    oldPrice: 1099,
-    display: '6.1" OLED',
-    capacity: '128GB',
-    ram: '6GB',
-    imageSrc: '/images/iphone-13.png',
-  },
 ];
