6d18483521f3eef2e639de86acfc31893e088f27
Dima Ilchenko <dima.ilchenko.ol@gmail.com>
2025-09-20 14:53:59 +0200

    add some animations

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,48 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import styles from './footer.module.scss';
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Footer() {
+  const footerRef = useRef<HTMLElement | null>(null);
+  const logoRef = useRef<HTMLAnchorElement | null>(null);
+  const navRef = useRef<HTMLUListElement | null>(null);
+  const rightRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: footerRef.current,
+          start: 'top 95%',
+          toggleActions: 'play none none reverse',
+        },
+        defaults: { ease: 'power3.out', duration: 0.8 },
+      });
+
+      tl.from(footerRef.current, { opacity: 0, scale: 0.95 })
+
+        .from(logoRef.current, { y: 20, opacity: 0 }, '-=0.3')
+        .from(
+          navRef.current?.children || [],
+          { y: 20, opacity: 0, stagger: 0.15 },
+          '-=0.2',
+        )
+        .from(rightRef.current, { y: 20, opacity: 0 }, '-=0.2');
+    });
+
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <footer className={styles.footer}>
-      <div className={styles.footerContent}>
-        <Link href="/">
+    <footer className="footer" ref={footerRef}>
+      <div className="footer__content">
+        <Link href="/" ref={logoRef}>
           <Image
             src="/assets/logo.png"
             className={styles.footerLogo}
@@ -16,8 +52,8 @@ export default function Footer() {
           />
         </Link>
 
-        <nav className={styles.footerNav}>
-          <ul className={styles.footerLists}>
+        <nav className="footer__nav">
+          <ul ref={navRef} className="footer__lists">
             <li>
               <Link href="github" className={styles.footerNavLink}>
                 Github
@@ -36,9 +72,9 @@ export default function Footer() {
           </ul>
         </nav>
 
-        <div className={styles.footerRight}>
-          <p className={styles.footerRightText}>Back to top</p>
-          <button className={styles.footerButtonTop}>↑</button>
+        <div ref={rightRef} className="footer__right">
+          <p className="footer__right-text small-text">Back to top</p>
+          <button className="footer__button-top">q</button>
         </div>
       </div>
     </footer>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,15 +2,17 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import classNames from 'classnames';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { usePathname } from 'next/navigation';
+import gsap from 'gsap';
 
-import FavouriteIcon from '../../assets/icons/favourite-default.svg';
-import ShopCart from '../../assets/icons/cart-shopping.svg';
-import BurgerMenu from '../../assets/icons/menu-burger.svg';
-import Close from '../../assets/icons/close.svg';
 import styles from './header.module.scss';
 import { Menu } from './Menu/Menu';
+
+import FavouriteIcon from '@/assets/icons/favourite-default.svg';
+import ShopCart from '@/assets/icons/cart-shopping.svg';
+import BurgerMenu from '@/assets/icons/menu-burger.svg';
+import Close from '@/assets/icons/close.svg';
 
 export const Header = () => {
   const navLinks = [
@@ -23,75 +25,93 @@ export const Header = () => {
   const [isOpenedMenu, setIsOpenedMenu] = useState(false);
   const currentPath = usePathname();
 
+  const logoRef = useRef<HTMLDivElement | null>(null);
+  const navRef = useRef<HTMLUListElement | null>(null);
+  const rightRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const tl = gsap.timeline({
+        defaults: { ease: 'power3.out', duration: 0.8 },
+      });
+
+      tl.from(logoRef.current, { x: -50, opacity: 0 })
+        .from(
+          navRef.current?.children || [],
+          { y: -30, opacity: 0, stagger: 0.15 },
+          '-=0.4',
+        )
+        .from(rightRef.current, { y: -40, opacity: 0 }, '-=0.3');
+    });
+
+    return () => ctx.revert();
+  }, []);
+
   return (
     <>
-      <header className={styles.header}>
-        <div className={styles.headerContainer}>
+      <header className="header">
+        <div ref={logoRef}>
           <Link href="/">
             <Image
               width={80}
               height={26}
               src="/assets/logo.png"
-              className={styles.headerLogo}
+              className="header__logo"
               alt="Nice Gadgets"
             />
           </Link>
+        </div>
 
-          <nav className={styles.headerNav}>
-            <ul className={styles.headerLists}>
-              {navLinks.map((link) => (
-                <li key={link.path}>
-                  <Link
-                    href={link.path}
-                    className={classNames(
-                      styles.headerNavLink,
-                      'uppercase-text',
-                      {
-                        [styles.linkIsActive]: currentPath === link.path,
-                      },
-                    )}
-                  >
-                    {link.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
+        <nav className="header__nav">
+          <ul ref={navRef} className="header__lists">
+            {navLinks.map((link) => (
+              <li key={link.path}>
+                <Link
+                  className={classNames('header__nav-link uppercase-text', {
+                    'link-is-active': currentPath === link.path,
+                  })}
+                  href={link.path}
+                >
+                  {link.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
 
-          <div className={styles.headerRight}>
-            <div
-              className={classNames(styles.headerRightFavourites, {
-                [styles.linkIconIsActive]: currentPath === '/favorites',
-              })}
-            >
-              <Link href="/favorites">
-                <FavouriteIcon className={styles.headerIcon} />
-              </Link>
-            </div>
+        <div ref={rightRef} className="header__right">
+          <div
+            className={classNames('header__right-favourites', {
+              'link__icon-is-active': currentPath === '/favorites',
+            })}
+          >
+            <Link href="/favorites">
+              <FavouriteIcon className={styles.headerIcon} />
+            </Link>
+          </div>
 
-            <div
-              className={classNames(styles.headerRightCart, {
-                [styles.linkIconIsActive]: currentPath === '/shopcart',
-              })}
-            >
-              <Link href="/shopcart">
-                <ShopCart className={styles.headerIcon} />
-              </Link>
-            </div>
+          <div
+            className={classNames(styles.headerRightCart, {
+              [styles.linkIconIsActive]: currentPath === '/shopcart',
+            })}
+          >
+            <Link href="/shopcart">
+              <ShopCart className={styles.headerIcon} />
+            </Link>
+          </div>
 
-            <div className={styles.headerBurgerMenu}>
-              {!isOpenedMenu ? (
-                <BurgerMenu
-                  className={styles.headerIcon}
-                  onClick={() => setIsOpenedMenu(true)}
-                />
-              ) : (
-                <Close
-                  className={styles.headerIcon}
-                  onClick={() => setIsOpenedMenu(false)}
-                />
-              )}
-            </div>
+          <div className={styles.headerBurgerMenu}>
+            {!isOpenedMenu ? (
+              <BurgerMenu
+                className={styles.headerIcon}
+                onClick={() => setIsOpenedMenu(true)}
+              />
+            ) : (
+              <Close
+                className={styles.headerIcon}
+                onClick={() => setIsOpenedMenu(false)}
+              />
+            )}
           </div>
         </div>
       </header>
