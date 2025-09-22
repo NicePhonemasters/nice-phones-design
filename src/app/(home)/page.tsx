77266202a2c001088443de9b43145fd70b96c497
Dima Ilchenko <dima.ilchenko.ol@gmail.com>
2025-09-22 19:55:49 +0200

    prepared branch with adv animations

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,10 @@
 import { HomePage } from '../../components/HomePage/HomePage';
+import { Loader } from '@components/Loader/Loader';
 
 export default function App() {
   return (
     <div>
+      <Loader />
       <HomePage />
     </div>
   );
