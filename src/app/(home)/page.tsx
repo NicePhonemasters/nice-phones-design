34f3656cf4aa2f0ca4e799488001dde7c013bea3
Dima Ilchenko <dima.ilchenko.ol@gmail.com>
2025-09-22 20:47:08 +0200

    check

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,8 @@
 import { HomePage } from '../../components/HomePage/HomePage';
-import { Loader } from '@components/Loader/Loader';
 
 export default function App() {
   return (
     <div>
-      <Loader />
       <HomePage />
     </div>
   );
