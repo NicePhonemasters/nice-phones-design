98b17e1a4be1c2e70f4651a7ed4cef66ef32a77e
NicssLucik02 <nikitalucenko1@gmail.com>
2025-09-23 19:14:03 +0300

    fixed product-card

--- src/app/catalog/[category]/page.tsx
+++ src/app/catalog/[category]/page.tsx
@@ -7,14 +7,8 @@ import styles from './Catalog.module.scss';
 import { PaginationControl } from '@components/ui/Controls/PaginationControl';
 import { DropdownSort } from '@components/ui/Dropdowns/DropdownSort';
 import { DropdownPages } from '@components/ui/Dropdowns/DropdownPages';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import ProductCard from '@components/ProductCard/ProductCard';
 
 const Catalog: React.FC = () => {
-  //TODO: rework
-  // const [sortBy, setSortBy] = useState('new');
-  // const [itemsPerPage, setItemsPerPage] = useState(16);
-
   const SelectItem = React.forwardRef<
     HTMLDivElement,
     React.ComponentPropsWithoutRef<typeof Select.Item>
