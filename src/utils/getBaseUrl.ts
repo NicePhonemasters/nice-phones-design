7c87ea784aa80f89d5f22e58d4245aa3a03ecd88
Maxim <makson202007@gmail.com>
2025-09-24 11:07:34 +0300

    fix: deploy issues

--- src/utils/getBaseUrl.ts
+++ src/utils/getBaseUrl.ts
@@ -1,8 +1,13 @@
 export function getBaseUrl() {
+  if (typeof window === 'undefined') {
+    return '';
+  }
+
   const vercelUrl = process.env.VERCEL_URL;
 
   if (vercelUrl) {
     return `https://${vercelUrl}`;
   }
+
   return 'http://localhost:3000';
 }
